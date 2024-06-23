ab11765ed5df7ef46065df7a6faf889f4efdf23f
Alex <alexandrumoldoveanu97@gmail.com>
2024-06-23 14:00:08 +0300

    Add files

---
+++ Rule5 - Copy.c
@@ -0,0 +1,228 @@
+#include <stdint.h>
+#include "Rule5.h"
+
+void Rule5(void)
+{
+#ifdef Rule_5_1
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa3 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa4 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa5 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa6 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa7 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa8 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa9 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa0 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa1 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa2 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa31 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa42 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa53 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa64 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa75 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa86 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa97 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa08 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa19 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa20 = 1U;
+#endif
+
+#ifdef Rule_5_1_1
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa3 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa4 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa5 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa6 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa7 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa8 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa9 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa0 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa1 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa2 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa31 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa42 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa53 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa64 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa75 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa86 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa97 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa08 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa19 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa20 = 1U;
+#endif
+
+#ifdef Rule_5_2
+	uin32_t var1 = 5U;
+	{
+		uint32_t var1 = 6U;
+	}
+	uin32_t var2 = 5U;
+	{
+		uint32_t var2 = 6U;
+	}
+	uin32_t var3 = 5U;
+	{
+		uint32_t var3 = 6U;
+	}
+	uin32_t var4 = 5U;
+	{
+		uint32_t var4 = 6U;
+	}
+	uin32_t var5 = 5U;
+	{
+		uint32_t var1 = 6U;
+	}
+	uin32_t var6 = 5U;
+	{
+		uint32_t var2 = 6U;
+	}
+	uin32_t var7 = 5U;
+	{
+		uint32_t var3 = 6U;
+	}
+	uin32_t var8 = 5U;
+	{
+		uint32_t var4 = 6U;
+	}
+	uin32_t var12 = 5U;
+	{
+		uint32_t var1 = 6U;
+	}
+	uin32_t var22 = 5U;
+	{
+		uint32_t var2 = 6U;
+	}
+	uin32_t var32 = 5U;
+	{
+		uint32_t var3 = 6U;
+	}
+#endif
+
+#ifdef Rule_5_2_2
+	uin32_t var1 = 5U;
+	{
+		uint32_t var1 = 6U;
+	}
+	uin32_t var2 = 5U;
+	{
+		uint32_t var2 = 6U;
+	}
+	uin32_t var3 = 5U;
+	{
+		uint32_t var3 = 6U;
+	}
+	uin32_t var4 = 5U;
+	{
+		uint32_t var4 = 6U;
+	}
+	uin32_t var5 = 5U;
+	{
+		uint32_t var1 = 6U;
+	}
+	uin32_t var6 = 5U;
+	{
+		uint32_t var2 = 6U;
+	}
+	uin32_t var7 = 5U;
+	{
+		uint32_t var3 = 6U;
+	}
+	uin32_t var8 = 5U;
+	{
+		uint32_t var4 = 6U;
+	}
+	uin32_t var12 = 5U;
+	{
+		uint32_t var1 = 6U;
+	}
+	uin32_t var22 = 5U;
+	{
+		uint32_t var2 = 6U;
+	}
+	uin32_t var32 = 5U;
+	{
+		uint32_t var3 = 6U;
+	}
+#endif
+	
+#ifdef Rule_5_3
+	{
+		typedef char char_t;
+	}
+	{
+		typedef unsigned char char_t;
+	}
+	{
+		typedef char char_t;
+	}
+	{
+		typedef unsigned char char_t;
+	}
+#endif
+
+#ifdef Rule_5_3
+	{
+		typedef char char_t;
+	}
+	{
+		typedef unsigned char char_t;
+	}
+	{
+		typedef char char_t;
+	}
+	{
+		typedef unsigned char char_t;
+	}
+#endif
+
+eroare aici
+
+#ifdef Rule_5_3_2
+	{
+		typedef char char_t_t;
+	}
+	{
+		typedef unsigned char char_t_t;
+	}
+#endif
+	
+#ifdef Rule_5_3_1
+	{
+		typedef char char_t_1;
+	}
+	{
+		typedef unsigned char char_t_1;
+	}
+#endif
+
+
+#ifdef Rule_5_4
+	#warning "Rule 5.4(required) is not checked by IAR MISRA C tool
+	#warning "A tag name shall be a unique identifier."
+#endif
+	
+#ifdef Rule_5_5
+	#warning "Rule 5.5(advisory) is not checked by IAR MISRA C tool
+	#warning "No object or function identifier with static storage duration should be reused."
+#endif
+
+	
+#ifdef Rule_5_7
+	#warning "Rule 5.7(advisory) is not checked by IAR MISRA C tool
+	#warning "No identifier name should be reused."
+#endif	
+}
+#ifdef Rule_5_4_1
+	#warning "Rule 5.4(required) is not checked by IAR MISRA C tool
+	#warning "A tag name shall be a unique identifier."
+#endif
+
+
+#ifdef Rule_5_6_1
+	#warning "Rule 5.6(advisory) is not checked by IAR MISRA C tool
+	#warning "No identifier in one namespace should have the same spelling as an identifier in another namespace, with the exception of structure member and union member names."
+#endif
+	
+#ifdef Rule_5_7_1
+	#warning "Rule 5.7(advisory) is not checked by IAR MISRA C tool
+	#warning "No identifier name should be reused."
+#endif	
+}
