e319a113f9284790466d7c87ae36137efb3ae951
Alex <alexandrumoldoveanu97@gmail.com>
2024-06-23 13:49:37 +0300

    Add last version

--- Rule5.c
+++ Rule5.c
@@ -14,6 +14,16 @@ void Rule5(void)
 	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa0 = 1U;
 	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa1 = 0U;
 	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa2 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa31 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa42 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa53 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa64 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa75 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa86 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa97 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa08 = 1U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa19 = 0U;
+	uint32_t abcdefghijklmnopqrstuvwxyz123456789aaa20 = 1U;
 #endif
 	
 #ifdef Rule_5_2
@@ -49,6 +59,38 @@ void Rule5(void)
 	{
 		uint32_t var4 = 6U;
 	}
+	uin32_t var12 = 5U;
+	{
+		uint32_t var1 = 6U;
+	}
+	uin32_t var22 = 5U;
+	{
+		uint32_t var2 = 6U;
+	}
+	uin32_t var32 = 5U;
+	{
+		uint32_t var3 = 6U;
+	}
+	uin32_t var42 = 5U;
+	{
+		uint32_t var4 = 6U;
+	}
+	uin32_t var52 = 5U;
+	{
+		uint32_t var1 = 6U;
+	}
+	uin32_t var62 = 5U;
+	{
+		uint32_t var2 = 6U;
+	}
+	uin32_t var72 = 5U;
+	{
+		uint32_t var3 = 6U;
+	}
+	uin32_t var82 = 5U;
+	{
+		uint32_t var4 = 6U;
+	}
 #endif
 	
 #ifdef Rule_5_3
@@ -61,6 +103,30 @@ void Rule5(void)
 #endif
 
 #ifdef Rule_5_3_2
+	{
+		typedef char char_t_t;
+	}
+	{
+		typedef unsigned char char_t_t;
+	}
+#endif
+#ifdef Rule_5_3_9999
+	{
+		typedef char char_t_t;
+	}
+	{
+		typedef unsigned char char_t_t;
+	}
+#endif
+#ifdef Rule_5_3_999
+	{
+		typedef char char_t_t;
+	}
+	{
+		typedef unsigned char char_t_t;
+	}
+#endif
+#ifdef Rule_5_3_99
 	{
 		typedef char char_t_t;
 	}
@@ -95,6 +161,26 @@ void Rule5(void)
 #endif
 	
 #ifdef Rule_5_7
+	#warning "Rule 5.7(advisory) is not checked by IAR MISRA C tool
+	#warning "No identifier name should be reused."
+#endif	
+}
+#ifdef Rule_5_4_1
+	#warning "Rule 5.4(required) is not checked by IAR MISRA C tool
+	#warning "A tag name shall be a unique identifier."
+#endif
+	
+#ifdef Rule_5_5_1
+	#warning "Rule 5.5(advisory) is not checked by IAR MISRA C tool
+	#warning "No object or function identifier with static storage duration should be reused."
+#endif
+
+#ifdef Rule_5_6_1
+	#warning "Rule 5.6(advisory) is not checked by IAR MISRA C tool
+	#warning "No identifier in one namespace should have the same spelling as an identifier in another namespace, with the exception of structure member and union member names."
+#endif
+	
+#ifdef Rule_5_7_1
 	#warning "Rule 5.7(advisory) is not checked by IAR MISRA C tool
 	#warning "No identifier name should be reused."
 #endif	
